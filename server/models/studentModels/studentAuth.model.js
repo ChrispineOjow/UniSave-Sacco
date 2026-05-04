f96ed71c93a2e3d5dd3778507d2a7b742f2c7b1c
Chrispine Ojow <WameyoChrispine@gmail.com>
2026-05-05 00:37:17 +0300

    developing the Model

---
+++ server/models/studentModels/studentAuth.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const studentAuthSchema = new mongoose.Schema({
+
+    nationalId:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength: 6,
+
+    },
+    accountStatus:{
+        type:String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: 'pending'
+    }
+}, { timestamps: true});
+
+//Auto update email in student profile when email in student auth is updated
+studentAuthSchema.post('findOneAndUpdate', async function(doc){
+    if(doc){
+        await mongoose.model('student_profile').findOneAndUpdate(
+            { studentAuthId: doc._id},
+            {email: doc.email}
+        );
+    }
+});
+
+const StudentAuth = mongoose.model('student_auth', studentAuthSchema);
+
+export default StudentAuth;
---
+++ server/models/studentModels/studentProfile.model.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+const studentProfileSchema = new mongoose.Schema({
+    studentAuthId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'student_auth',
+        required: true,
+        unique: true
+    },
+    nationalId:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    surName:{
+        type:String,
+        required:true
+    },
+    email: {
+        required: true,
+        unique: true
+    },
+    university:{
+        type: String,
+        required: true
+    },
+    course:{
+        type: String,
+        required: true
+    },
+    schoolRegistrationNumber:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    MTI_Score:{
+        type: Number,
+        required: true
+    },
+    phoneNumber:{
+        type: String,
+        required: true,
+        unique: true
+    }
+    
+},  { timestamps: true})
+
+const StudentProfile = mongoose.model('student_profile', studentProfileSchema);
+export default StudentProfile;
