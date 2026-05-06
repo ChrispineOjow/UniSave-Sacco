f5bf31be1760aec4744321186ba205ad15b727fc
Chrispine Ojow <WameyoChrispine@gmail.com>
2026-05-06 12:18:10 +0300

    modified server.js to prevent th MaxListenersExceedingWarning

--- server/server.js
+++ server/server.js
@@ -11,13 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(express.json());
+
 //Routes
 app.get('/', (req, res)=>{
     res.json({ message: 'Welcome to UniSave Sacco System API' });
 })
 app.use('/api/students/auth', studentAuthRouter);
 app.use('/api/students/profile', studentProfileRouter);
-
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
@@ -53,12 +53,7 @@ connectDB().then(()=>{
         cleanup('unhandledRejection');
     });
 
-
-
 }).catch((error)=>{
     console.log('DB connection failed:  ', error.message);
     process.exit(1);
-})
-
-
-
+});
