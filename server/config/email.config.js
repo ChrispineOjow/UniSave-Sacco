33280d0015f1713d0402220e74137c9d90b64679
Chrispine Ojow <WameyoChrispine@gmail.com>
2026-06-08 17:29:11 +0300

    Fixed an issue with the email notification system

--- server/config/email.config.js
+++ server/config/email.config.js
@@ -2,11 +2,14 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport(
     {
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth:{
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        family: 4
     }
 );
 
